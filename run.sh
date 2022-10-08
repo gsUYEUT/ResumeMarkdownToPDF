814231c3b7261f8fc6fa92ccffa52d3ef9b2b17a
user <user@github.com>
2022-10-07 22:29:33 -0400

    update

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ if [[ ! $1 ]]; then
 fi
 (set -x && hash pandoc)
 (set -x && hash firefox)
-scriptDir="$(cd "$(dirname $(readlink -f "${BASH_SOURCE[0]}"))" && pwd)"
-cd "$scriptDir"
-(set -x && pandoc --css=style.css "$1" -f markdown -t html -s -o resume.html)
+scriptDir="$( cd "$( dirname $(readlink -f "${BASH_SOURCE[0]}" ) )" && pwd )"
+
+(set -x && pandoc --css="$scriptDir/style.css" "$1" -f markdown -t html -s -o resume.html)
 echo -e 'firefox resume.html #\033[1m\033[38;2;255;255;255m open resume.html in firefox and press ctl+p to print to pdf\033[0m'
 # [[ -f ~/Downloads/resume.pdf ]] && firefox ~/Downloads/resume.pdf
